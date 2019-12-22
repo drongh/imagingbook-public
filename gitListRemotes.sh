355d127dc8d8087639479cec9478088089a3a34f
imagingbook <wilhelm.burger@fh-hagenberg.at>
2019-12-22 21:41:27 +0100

    cleanup

--- gitListRemotes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Listing remote GIT repos:"
-echo ""
-git remote -v
-git submodule foreach --recursive 'git remote -v'
-echo ""
-read -rsp $'Done. Press any key to quit...\n' -n1 key
